9d5f3420a32a4db8de67b9cc2b685f5d08cee2cf
t1mosha <felix-chan@mail.ru>
2021-11-17 03:12:53 +0400

    first.c

---
+++ first.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+void main()
+		{
+			int a,b;
+			char c;        
+	printf("Чо над сделац?" + - \ * "\ n");
+while(l)
+	{
+	printf("Оператор ввода:");
+	scanf("%c" ,&c);
+	printf("Введи два числа:");
+	scanf("%d %d",&a,&b);
+switch(c)
+	{
+		case '+' :
+		printf("%d+%d=%d\n",a,b,a+b);
+	break;
+			case '-' :
+			printf("%d-%d=%d\n",a,b,a-b);
+		break;
+				case "*" :
+				printf("%d*%d=%d\n",a,b,a*b);
+			break;
+					case "/":
+					printf("%d\%d=%d\n",a,b,a/b);
+				break;
+					default:
+		printf("input error\n");
+                      }
+getchar();
+	}
+		}
+
